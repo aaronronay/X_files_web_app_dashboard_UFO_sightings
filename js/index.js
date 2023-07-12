69fc31d2cf4f30b36f408d8b2d7f21eee64ad90b
aaronronay <30646034+aaronronay@users.noreply.github.com>
2023-07-11 23:27:18 -0400

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,157 +1,73 @@
-// Get references to the elements of the DOM
-var $tbody = document.querySelector("tbody");
-var $dateTimeInput = document.querySelector("#date_time");
-var $cityInput = document.querySelector("#city");
-var $stateInput = document.querySelector("#state");
-var $countryInput = document.querySelector("#country");
-var $shapeInput = document.querySelector("#shape");
-var $searchBtn = document.querySelector("#search");
-var $recordCounter = document.querySelector("#recordCounter");
-var $pages = document.querySelector("#pages");
-var $loadBtn = document.querySelector("#load");
-var $nextBtn = document.querySelector("#next");
-var $prevBtn = document.querySelector("#prev");
+const $tbody = document.querySelector("tbody");
+const $inputs = Object.fromEntries(
+  ["dateTime", "city", "state", "country", "shape"].map((key) => [
+    key,
+    document.querySelector(`#${key}`)
+  ])
+);
+const $searchBtn = document.querySelector("#search");
+const $recordCounter = document.querySelector("#recordCounter");
+const $pages = document.querySelector("#pages");
+const $loadBtn = document.querySelector("#load");
+const $nextBtn = document.querySelector("#next");
+const $prevBtn = document.querySelector("#prev");
 
-// Add event listeners
-$searchBtn.addEventListener("click", handleSearchButtonClick);
-$loadBtn.addEventListener("click", handleReloadButtonClick);
-$nextBtn.addEventListener("click", handleNextButtonClick);
-$prevBtn.addEventListener("click", handlePrevButtonClick);
-$pages.addEventListener("change", handlePagesChange);
+let filteredData = dataSet;
+let count = 0;
 
-// Initialize global variables
-var filteredData = dataSet;
-var count = 0;
+$searchBtn.addEventListener("click", handleSearch);
+$loadBtn.addEventListener("click", handleReload);
+$nextBtn.addEventListener("click", () => handlePagination(1));
+$prevBtn.addEventListener("click", () => handlePagination(-1));
+$pages.addEventListener("change", renderTable);
 
-// Define Event handler functions
-// handleNextButtonClick increments count and renders
-function handleNextButtonClick() {
-    count++;
-    renderTable();
-}
-// handlePrevButtonClick decrements count and renders
-function handlePrevButtonClick() {
-    count--;
-    renderTable();
-}
-
-// handlePagesChange renders for new record count selected
-function handlePagesChange() {
-    renderTable();
-}
-
-// handleSearchButtonClick handles search button click:
-//    cleans input data
-//    checks for non-empty search fields and adds to filter
-//    renders table
-function handleSearchButtonClick() {
-    var filterDate = $dateTimeInput.value.trim();
-    var filterCity = $cityInput.value.trim().toLowerCase();
-    var filterState = $stateInput.value.trim().toLowerCase();
-    var filterCountry = $countryInput.value.trim().toLowerCase();
-    var filterShape = $shapeInput.value.trim().toLowerCase();
-
-    if (filterDate != "") {
-        filteredData = filteredData.filter(function (date) {
-        var dataDate = date.datetime;
-        return dataDate === filterDate;
-    });
-
-    }
-
-    if (filterCity != "") {
-        filteredData = filteredData.filter(function (city) {
-        var dataCity = city.city;
-        return dataCity === filterCity;
-    });
-    }
-
-    if (filterState != "") {
-        filteredData = filteredData.filter(function (state) {
-            var dataState = state.state;
-            return dataState === filterState;
-        });
-    }
-
-    if (filterCountry != "") {
-        filteredData = filteredData.filter(function (country) {
-            var dataCountry = country.country;
-            return dataCountry === filterCountry;
-        });
-    }
-
-    if (filterShape != "") {
-        filteredData = filteredData.filter(function (shape) {
-            var dataShape = shape.shape;
-            return dataShape === filterShape;
-        });
-    }
-
-    renderTable();
-}
-
-// handleReloadButtonClick resets count and search fields, and renders
-function handleReloadButtonClick() {
-    count = 0;
-    filteredData = dataSet;
-    $dateTimeInput.value = '';
-    $cityInput.value = '';
-    $stateInput.value = '';
-    $countryInput.value = '';
-    $shapeInput.value = '';
-
-    renderTable();
-}
-
-// Define renderTable function
-function renderTable() {
-    // clear previously rendered table
-    $tbody.innerHTML = "";
-
-    // Get number of records to be rendered
-    var pages = Number(document.getElementById("pages").value);
-
-    // Initialize local variables
-    var start = count * pages + 1;
-    var end = start + pages - 1;
-    var btn;
-
-    // Adjusts records displayed for end of data and state of Next button
-    if (end > filteredData.length) {
-      end = filteredData.length;
-      btn = document.getElementById("next");
-      btn.disabled = true;
-    }
-    else {
-      btn = document.getElementById("next");
-      btn.disabled = false;
-    }
-
-    // Adjusts state of Previous button
-    if (start == 1) {
-      btn = document.getElementById("prev");
-      btn.disabled = true;
-    }
-    else {
-      btn = document.getElementById("prev");
-      btn.disabled = false;
-    }
-
-    // Displays record counts and loads records into table
-    $recordCounter.innerText = "From Record: " + start + " to: " + end + " of " + filteredData.length;
-    // Outer loop loads specified number of records
-    for (var i = 0; i < pages; i++) {
-        var item = filteredData[i+(count * pages)];
-        var fields = Object.keys(item);
-        var $row = $tbody.insertRow(i);
-        // Inner loop loads fields in record
-        for (var j = 0; j < fields.length; j++) {
-            var field = fields[j];
-            var $cell = $row.insertCell(j);
-            $cell.innerText = item[field];
-        }
-    }
-}
-
-// Provides initial render on open
 renderTable();
+
+function handlePagination(direction) {
+  count += direction;
+  renderTable();
+}
+
+function handleSearch() {
+  const filters = Object.fromEntries(
+    Object.entries($inputs).map(([key, input]) => [
+      key,
+      input.value.trim().toLowerCase()
+    ])
+  );
+
+  filteredData = dataSet.filter((data) =>
+    Object.entries(filters).every(([key, value]) => data[key] === value)
+  );
+
+  renderTable();
+}
+
+function handleReload() {
+  count = 0;
+  filteredData = dataSet;
+  Object.values($inputs).forEach((input) => (input.value = ""));
+  renderTable();
+}
+
+function renderTable() {
+  const pages = Number($pages.value);
+  const start = count * pages;
+  const end = start + pages;
+  const dataSubset = filteredData.slice(start, end);
+
+  $tbody.innerHTML = "";
+  $nextBtn.disabled = end >= filteredData.length;
+  $prevBtn.disabled = start === 0;
+  $recordCounter.textContent = `From Record: ${start + 1} to: ${Math.min(
+    end,
+    filteredData.length
+  )} of ${filteredData.length}`;
+
+  dataSubset.forEach((data) => {
+    const $row = $tbody.insertRow();
+    Object.values(data).forEach((value) => {
+      $row.insertCell().textContent = value;
+    });
+  });
+}
